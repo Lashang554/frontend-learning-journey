81d7e73c76a5af4650abb51b2783d31b555d01b2
Lashang544 <lasanglama554@gmail.com>
2026-04-03 20:18:49 +0545

    Get input values and log

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get values from inputs (for testing)
+function calculate(type) {
+
+  // 1. Get input values
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+
+  // 2. Print in console
+  console.log(num1, num2, type);
+}
