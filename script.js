7d5a1c650f6a91834a69c5be16ded609e8e11f98
Lashang544 <lasanglama554@gmail.com>
2026-04-03 20:20:00 +0545

    Day 05 - Add calculation logic

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// Get values from inputs (for testing)
 function calculate(type) {
 
-  // 1. Get input values
-  let num1 = document.getElementById("num1").value;
-  let num2 = document.getElementById("num2").value;
+  // 1. Convert values to number
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
 
-  // 2. Print in console
-  console.log(num1, num2, type);
+  // 2. Store result
+  let result;
+
+  // 3. Perform operation
+  if (type === "add") {
+    result = num1 + num2;
+  } 
+  else if (type === "sub") {
+    result = num1 - num2;
+  } 
+  else if (type === "mul") {
+    result = num1 * num2;
+  } 
+  else if (type === "div") {
+    result = num1 / num2;
+  }
+
+  // 4. Log result (for now)
+  console.log(result);
 }
