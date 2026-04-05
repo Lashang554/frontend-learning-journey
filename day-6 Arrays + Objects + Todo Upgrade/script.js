4d9d1ba32df2c86229b168f248830a4a70adc31a
Lashang544 <lasanglama554@gmail.com>
2026-04-05 22:52:10 +0545

    Add delete task feature

--- day-6 Arrays + Objects + Todo Upgrade/script.js
+++ day-6 Arrays + Objects + Todo Upgrade/script.js
@@ -1,17 +1,44 @@
-// 1. Create array to store tasks
 let tasks = [];
 
-// 2. Add task function
 function addTask() {
   let input = document.getElementById("taskInput");
   let value = input.value;
 
   if (value === "") return;
 
-  // 3. Add task to array
   tasks.push(value);
-
-  console.log(tasks);
-
   input.value = "";
+
+  displayTasks();
+}
+
+function displayTasks() {
+  let list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+
+    let li = document.createElement("li");
+    li.className = "flex justify-between bg-gray-200 p-2 rounded";
+
+    // Task text
+    let span = document.createElement("span");
+    span.innerText = tasks[i];
+
+    // Delete button
+    let btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.className = "text-red-500";
+
+    // 5. Delete logic
+    btn.onclick = function () {
+      tasks.splice(i, 1); // remove item
+      displayTasks();     // refresh UI
+    };
+
+    li.appendChild(span);
+    li.appendChild(btn);
+
+    list.appendChild(li);
+  }
 }
