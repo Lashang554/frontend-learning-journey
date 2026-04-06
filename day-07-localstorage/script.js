5af15eae12946f6e3476842b5ddf4207bed12ac8
Lashang544 <lasanglama554@gmail.com>
2026-04-06 22:11:48 +0545

    Display tasks using loop

--- day-07-localstorage/script.js
+++ day-07-localstorage/script.js
@@ -1,7 +1,22 @@
-// 1. Store tasks
+// // 1. Store tasks
+// let tasks = [];
+
+// // 2. Add task
+// function addTask() {
+//   let input = document.getElementById("taskInput");
+//   let value = input.value;
+
+//   if (value === "") return;
+
+//   tasks.push(value);
+
+//   console.log(tasks); // check in console
+
+//   input.value = "";
+// }
+
 let tasks = [];
 
-// 2. Add task
 function addTask() {
   let input = document.getElementById("taskInput");
   let value = input.value;
@@ -10,7 +25,21 @@ function addTask() {
 
   tasks.push(value);
 
-  console.log(tasks); // check in console
-
   input.value = "";
+
+  displayTasks(); // show tasks
+}
+
+// 3. Display tasks
+function displayTasks() {
+  let list = document.getElementById("taskList");
+
+  list.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = tasks[i];
+
+    list.appendChild(li);
+  }
 }
