4a00dc91e17809d08bc799df2a618a3eb07c73a8
Lashang544 <lasanglama554@gmail.com>
2026-04-08 21:31:15 +0545

    Add decrease button and styling

--- day-08-react/app/page.tsx
+++ day-08-react/app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>Counter App</h1>
 
-      <h2>0</h2>
+      <h2>{count}</h2>
 
-      <button>Increase</button>
+      <button onClick={() => setCount(count + 1)}>
+        ➕ Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        ➖ Decrease
+      </button>
     </div>
   );
 }
