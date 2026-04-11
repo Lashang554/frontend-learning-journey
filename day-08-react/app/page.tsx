fdcd5fa0dee9789630f95ba151a14cd5f168c2e4
Lashang544 <lasanglama554@gmail.com>
2026-04-11 21:59:53 +0545

    Day 09 - Setup todo UI

--- day-08-react/app/page.tsx
+++ day-08-react/app/page.tsx
@@ -1,23 +1,39 @@
+// "use client";
+
+// import { useState } from "react";
+
+// export default function Home() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div style={{ padding: "20px", textAlign: "center" }}>
+//       <h1>Counter App</h1>
+
+//       <h2>{count}</h2>
+
+//       <button onClick={() => setCount(count + 1)}>
+//         ➕ Increase
+//       </button>
+
+//       <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+//         ➖ Decrease
+//       </button>
+//     </div>
+//   );
+// }
+
+
+
+
 "use client";
 
-import { useState } from "react";
-
 export default function Home() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Counter App</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>Todo App</h1>
 
-      <h2>{count}</h2>
-
-      <button onClick={() => setCount(count + 1)}>
-        ➕ Increase
-      </button>
-
-      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
-        ➖ Decrease
-      </button>
+      <input placeholder="Enter task" />
+      <button>Add</button>
     </div>
   );
 }
