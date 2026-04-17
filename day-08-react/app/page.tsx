18ae556d1d4d9ce7e100d4a092a1a237c8c270d3
Lashang544 <lasanglama554@gmail.com>
2026-04-17 21:32:39 +0545

    Day 12 - Add filter and search state

--- day-08-react/app/page.tsx
+++ day-08-react/app/page.tsx
@@ -105,6 +105,119 @@
 
 
 
+// "use client";
+
+// import { useState, useEffect } from "react";
+
+// export default function Home() {
+//   const [task, setTask] = useState<string>("");
+//   type Task = {
+//     text: string;
+//     completed: boolean;
+//   };
+
+//   const [tasks, setTasks] = useState<Task[]>([]);
+
+//   const addTask = () => {
+//     if (task.trim() === "") return;
+
+//     setTasks([...tasks, { text: task, completed: false }]);
+//     setTask("");
+//   };
+
+//   const deleteTask = (index: number) => {
+//     setTasks(tasks.filter((_, i) => i !== index));
+//   };
+//   const toggleComplete = (index: number) => {
+//     const newTasks = [...tasks];
+//     newTasks[index].completed = !newTasks[index].completed;
+//     setTasks(newTasks);
+//   };
+
+//   // load tasks
+//   useEffect(() => {
+//     const saved = localStorage.getItem("tasks");
+//     if (saved) {
+//       setTasks(JSON.parse(saved));
+//     }
+//   }, []);
+
+//   // save tasks
+//   useEffect(() => {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   }, [tasks]);
+
+
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4">
+
+//       {/* Card */}
+//       <div className="w-full max-w-md bg-white/90 backdrop-blur-md rounded-2xl shadow-2xl p-6">
+
+//         {/* Title */}
+//         <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+//           📝 My Todo App
+//         </h1>
+
+//         {/* Input Section */}
+//         <div className="flex gap-2 mb-6 text-gray-900">
+//           <input
+//             value={task}
+//             onChange={(e) => setTask(e.target.value)}
+//             placeholder="Write a task..."
+//             className="flex-1 px-4 py-2 border rounded-xl outline-none focus:ring-2 focus:ring-purple-400"
+//           />
+
+//           <button
+//             onClick={addTask}
+//             className="px-5 py-2 bg-purple-600 text-white rounded-xl hover:bg-purple-700 transition"
+//           >
+//             Add
+//           </button>
+//         </div>
+
+//         {/* Task List */}
+//         <ul className="space-y-3">
+//           {tasks.length === 0 ? (
+//             <p className="text-center text-gray-500">
+//               No tasks yet 🚀 Add your first task
+//             </p>
+//           ) : (
+//             tasks.map((t, index) => (
+//               <li
+//                 key={index}
+//                 className="flex justify-between items-center bg-gray-100 p-3 rounded-xl shadow-sm hover:shadow-md transition"
+//               >
+//                 <span
+//                   onClick={() => toggleComplete(index)}
+//                   className={`cursor-pointer ${t.completed ? "line-through text-gray-400" : "text-gray-800"
+//                     }`}
+//                 >
+//                   {t.text}
+//                 </span>
+//                 <button
+//                   onClick={() => deleteTask(index)}
+//                   className="text-red-500 hover:text-red-700 font-bold"
+//                 >
+//                   ✖
+//                 </button>
+//               </li>
+//             ))
+//           )}
+//         </ul>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+// {🔍 Search tasks
+// 🎯 Filter (All / Active / Completed)
+// 🎨 Tabs UI}
+
 "use client";
 
 import { useState, useEffect } from "react";
@@ -115,6 +228,7 @@ export default function Home() {
     text: string;
     completed: boolean;
   };
+
 
   const [tasks, setTasks] = useState<Task[]>([]);
 
@@ -146,6 +260,10 @@ export default function Home() {
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
+  const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
+const [search, setSearch] = useState<string>("");
+
 
 
   return (
