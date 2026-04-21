64cdc42d06a316049885c463fd32f2eaeba4eb0b
Lashang544 <lasanglama554@gmail.com>
2026-04-21 22:17:43 +0545

    practice function

--- day-05-functions/script.js
+++ day-05-functions/script.js
@@ -1,32 +1,38 @@
-function calculate(type) {
+// function calculate(type) {
 
-  // 1. Get and convert values
-  let num1 = Number(document.getElementById("num1").value);
-  let num2 = Number(document.getElementById("num2").value);
+//   // 1. Get and convert values
+//   let num1 = Number(document.getElementById("num1").value);
+//   let num2 = Number(document.getElementById("num2").value);
 
-  // 2. Result variable
-  let result;
+//   // 2. Result variable
+//   let result;
 
-  // 3. Perform operation
-  if (type === "add") {
-    result = num1 + num2;
-  } 
-  else if (type === "sub") {
-    result = num1 - num2;
-  } 
-  else if (type === "mul") {
-    result = num1 * num2;
-  } 
-  else if (type === "div") {
+//   // 3. Perform operation
+//   if (type === "add") {
+//     result = num1 + num2;
+//   } 
+//   else if (type === "sub") {
+//     result = num1 - num2;
+//   } 
+//   else if (type === "mul") {
+//     result = num1 * num2;
+//   } 
+//   else if (type === "div") {
 
-    // 4. Handle divide by zero
-    if (num2 === 0) {
-      result = "Cannot divide by 0";
-    } else {
-      result = num1 / num2;
-    }
-  }
+//     // 4. Handle divide by zero
+//     if (num2 === 0) {
+//       result = "Cannot divide by 0";
+//     } else {
+//       result = num1 / num2;
+//     }
+//   }
 
-  // 5. Display result on screen
-  document.getElementById("result").innerText = "Result: " + result;
-}
+//   // 5. Display result on screen
+//   document.getElementById("result").innerText = "Result: " + result;
+// }
+
+
+
+
+
+
---
+++ simple_functions/script.js
@@ -0,0 +1,67 @@
+//simple function
+
+// function sayHello() {
+//     console.log("Hello!");
+// }
+
+// sayHello();
+
+
+//funtion with parameter
+// function age(number) {
+//     console.log('my age is ' + number);
+// }
+ 
+// age(25);
+
+
+//function with return value
+
+// function add(a,b) {
+//     return a + b ;
+// }
+
+// const result = add(10,20);
+
+// console.log(result);
+
+
+// function age(number) {
+// console.log('my age is  '+ number + ' years old');
+// }
+// age(30);
+
+// function name() {
+//     console.log("Lashang")
+// }
+// name();
+
+
+// function age(num) {
+//     console.log('lashang age is ' + num);
+// }
+// age(45);
+
+// function add (a,b){
+// return (a+b);
+// }
+
+// let result = add (13,12);
+// console.log(result);
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// let result = multiply(4, 5);
+// console.log(result);
+
+
+
+function lastName(str){
+return str . split (""). reverse () . join ("");
+}
+
+console.log (lastName ('Tamang'));
+
+
