7dd61694fd51b786ce3527952f1c2814712b1e18
Lashang544 <lasanglama554@gmail.com>
2026-04-27 22:03:51 +0545

    feat: render array of objects with map

--- mapping/react-practice/src/App.jsx
+++ mapping/react-practice/src/App.jsx
@@ -1,13 +1,41 @@
+// function App() {
+
+// const users = ["Ram","Hari","Sita"];
+
+// return (
+//   <div>
+//    {users.map((user,index)=>(
+//       <p key={index}>{user}</p>
+//    ))}
+//   </div>
+// )
+
+// }
+
+// export default App;
+
+
+// map object
 function App() {
 
-const users = ["Ram","Hari","Sita"];
+const users = [
+ {id:1,name:"Ram",age:20},
+ {id:2,name:"Hari",age:22},
+ {id:3,name:"Sita",age:19}
+];
 
 return (
-  <div>
-   {users.map((user,index)=>(
-      <p key={index}>{user}</p>
-   ))}
-  </div>
+<div>
+<h1>Users</h1>
+
+{users.map((user)=>(
+<div key={user.id}>
+<h3>{user.name}</h3>
+<p>{user.age}</p>
+</div>
+))}
+
+</div>
 )
 
 }
